b57092c371ab6db27fb80463c11ac69621b6077f
hansol <wkdqhrh00@naver.com>
2024-11-14 00:38:02 +0900

    chunk step

---
+++ src/main/kotlin/com/hansol/batchgi/docs/JobLauncherController.kt
@@ -0,0 +1,28 @@
+package com.hansol.batchgi.docs
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParameter
+import org.springframework.batch.core.JobParameters
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RestController
+class JobLauncherController(
+    private val jobLauncher: JobLauncher,
+    private val firstJob: Job,
+    private val secondJob: Job,
+) {
+
+    @PostMapping("/launch/first")
+    fun launch() {
+        // Running Jobs from within a Web Container
+        jobLauncher.run(firstJob, JobParameters(mapOf("seconds" to JobParameter(LocalDateTime.now().second, Int::class.java))))
+    }
+
+    @PostMapping("/launch/second")
+    fun launchSecond() {
+        jobLauncher.run(secondJob, JobParameters(mapOf("seconds" to JobParameter(LocalDateTime.now().second, Int::class.java))))
+    }
+}
---
+++ src/main/kotlin/com/hansol/batchgi/docs/SecondJobConfiguration.kt
@@ -0,0 +1,52 @@
+package com.hansol.batchgi.docs
+
+import com.hansol.batchgi.logger
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.support.ListItemReader
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+@Configuration
+class SecondJobConfiguration : DefaultBatchConfiguration() {
+
+    @Bean
+    fun secondJob(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Job {
+        return JobBuilder("secondJob", jobRepository)
+            .start(secondStep(jobRepository, transactionManager))
+            .build()
+    }
+
+    @Bean
+    fun secondStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("secondStep", jobRepository)
+            .chunk<String, String>(10, transactionManager)
+            .reader(itemReader())
+            .processor(itemProcessor())
+            .writer(itemWriter())
+            .build()
+    }
+
+    @Bean
+    fun itemReader(): ItemReader<String> {
+        return ListItemReader(listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"))
+    }
+
+    @Bean
+    fun itemProcessor(): ItemProcessor<String, String> {
+        return ItemProcessor { item -> "pro $item" }
+    }
+
+    @Bean
+    fun itemWriter(): ItemWriter<String> {
+        return ItemWriter { items -> logger.info { "## Write $items" }}
+    }
+}
